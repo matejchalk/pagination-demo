b154dd671f8d19a3f67f443f92319505d6f2acb2
Matěj Chalk <matej.chalk@flowup.cz>
2022-11-28 21:03:28 +0100

    Script for running db, server and client

---
+++ dev.js
@@ -0,0 +1,32 @@
+const concurrently = require('concurrently');
+const { exec } = require('child_process');
+
+concurrently(
+  [
+    {
+      name: 'server',
+      command:
+        'docker run -d --rm -p 27017:27017 --name mongo-test mongo:latest && npx nx serve server',
+    },
+    {
+      name: 'client',
+      command: 'npx nx serve client',
+    },
+  ],
+  {
+    prefix: 'name',
+    prefixColors: ['bgBlue.bold', 'bgMagenta.bold'],
+    killOthers: ['failure', 'success'],
+  }
+).result.then(stopMongoContainer, stopMongoContainer);
+
+function stopMongoContainer() {
+  exec('docker stop mongo-test', (err) => {
+    if (err) {
+      console.error('Failed to stop MongoDB container');
+    } else {
+      console.log('Stopped MongoDB container');
+    }
+    process.exit();
+  });
+}
